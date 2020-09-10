7b658681362a5a47b9e270363f85b8fe860250f2
Adnan Hakim <adnanhakim007@gmail.com>
2020-09-10 22:11:12 +0530

    Completed Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import './Home.css';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import Picture1 from '../Picture2.png';
 
 function Home() {
    return (
@@ -15,10 +16,29 @@ function Home() {
                   <div className="home-left-line"></div>
                </div>
                <div className="home-left-links">
-                  <InstagramIcon />
-                  <GitHubIcon />
-                  <LinkedInIcon />
+                  <a
+                     href="https://www.instagram.com/ad.nxn/"
+                     title="Follow me on Instagram"
+                     target="_blank"
+                     rel="noopener noreferrer">
+                     <InstagramIcon className="home-left-link" />
+                  </a>
+                  <a
+                     href="https://github.com/adnanhakim"
+                     title="See my projects on GitHub"
+                     target="_blank"
+                     rel="noopener noreferrer">
+                     <GitHubIcon className="home-left-link" />
+                  </a>
+                  <a
+                     href="https://www.linkedin.com/in/adnanhakim/"
+                     title="Follow me on LinkedIn"
+                     target="_blank"
+                     rel="noopener noreferrer">
+                     <LinkedInIcon className="home-left-link" />
+                  </a>
                </div>
+               <img src={Picture1} alt="" />
             </div>
             <div className="home-right">
                <div className="home-right-introduction">- introduction</div>
