1c306a2ba35a48f0ecea68adbe930bd5de26fcd1
Adnan Hakim <adnanhakim007@gmail.com>
2020-09-11 18:55:06 +0530

    Added firebase and fetched languages

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Projects.css';
 import Skill from './Skill';
 import AndroidIcon from '@material-ui/icons/Android';
 import WebIcon from '@material-ui/icons/Web';
 import DesktopMacIcon from '@material-ui/icons/DesktopMac';
+import { db } from '../firebase';
 
 function Projects() {
+   const [languages, setLanguages] = useState([]);
+
+   useEffect(() => {
+      async function fetchLanguages() {
+         const languagesRef = db.collection('languages');
+         const snapshot = await languagesRef.get();
+         if (snapshot.empty) {
+            console.log('No languages');
+         } else {
+            let languages = [];
+            snapshot.forEach((doc) => {
+               const data = doc.data();
+               languages.push({
+                  key: doc.id,
+                  name: data.name,
+                  url: data.url,
+               });
+            });
+            setLanguages(languages);
+         }
+      }
+      fetchLanguages();
+   }, []);
+
    return (
       <div className="projects-container">
          <div className="skills">
@@ -21,7 +46,15 @@ function Projects() {
                className="skill"
             />
          </div>
-         <div className="projects">Projects</div>
+
+         <div className="projects">
+            <div className="languages">
+               {languages.map((language) => (
+                  <img src={language.url} alt={language.name} />
+               ))}
+            </div>
+            Projects
+         </div>
       </div>
    );
 }
