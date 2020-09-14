37d686029deea3e77f65d53f2d15a4b491eaa73e
Adnan Hakim <adnanhakim007@gmail.com>
2020-09-15 00:32:29 +0530

    Added details

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Project.css';
 
-function Project({ title, language, image, link, details, css }) {
+function Project({ key, title, language, image, link, details, css }) {
+   const [classes, setClasses] = useState('project-back');
+
+   const toggle = () => {
+      if (classes === 'project-back') {
+         setClasses('project-back view-details');
+      } else setClasses('project-back');
+   };
+
    return (
-      <div data-aos="zoom-in" className="project-container">
-         <div className="project-columns">
+      <div data-aos="zoom-in" onClick={toggle} className="project-container">
+         <div className="project-front">
             <div className="project-info">
                <div className="project-title">{title}.</div>
                <div className="project-language">{language}.</div>
@@ -12,6 +20,10 @@ function Project({ title, language, image, link, details, css }) {
             <div className="project-button">visit.</div>
          </div>
          <img src={image} alt="" className={css} />
+         <div className={classes}>
+            <div className="project-body">{details}</div>
+            <div className="project-button">visit.</div>
+         </div>
       </div>
    );
 }
