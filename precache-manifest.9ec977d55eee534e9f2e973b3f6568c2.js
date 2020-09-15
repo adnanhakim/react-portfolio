33fdfda7c22d3797c2133c5088995361eb667a09
Adnan Hakim <adnanhakim007@gmail.com>
2020-09-15 16:20:39 +0530

    Updates

---
+++ precache-manifest.9ec977d55eee534e9f2e973b3f6568c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b948086ecca117d07cc7f91ffa9d28b",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "beb5af9c18ad3e9d1c35",
+    "url": "/react-portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "02c620e226ad936a2533",
+    "url": "/react-portfolio/static/css/main.7bd43766.chunk.css"
+  },
+  {
+    "revision": "beb5af9c18ad3e9d1c35",
+    "url": "/react-portfolio/static/js/2.6085f1a3.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/react-portfolio/static/js/2.6085f1a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02c620e226ad936a2533",
+    "url": "/react-portfolio/static/js/main.2ecbf106.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
